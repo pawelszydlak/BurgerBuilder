f09c7fd3207c40251c665ce3e9921d638978199c
pawelszydlak <35469335+pawelszydlak@users.noreply.github.com>
2018-06-14 20:15:13 +0200

    Added Toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,9 +18,10 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p><strong>Total price: {props.price.toFixed(2)}zł</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger">CANCEL</Button>
-            <Button btnType="Success">CONTINUE</Button>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </React.Fragment>
     );
 };
