9a90b83a351215e8455bbd928a39e5ce1b7941de
pawelszydlak <35469335+pawelszydlak@users.noreply.github.com>
2018-06-16 21:53:22 +0200

    Mobile improvements + sidedrawer toggle button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <React.Fragment>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () =>{
+        this.setState((perState) => {
+            return {showSideDrawer: !perState.showSideDrawer};
+        });
+    }
+
+    render () {
+        return (
+        <React.Fragment >
+            <Toolbar toggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </React.Fragment>
+        )
+    }
+}    
+
+export default Layout;
